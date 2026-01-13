6cb82fe2255c89b1f947e74fa4f79e5afb9fd7b4
lingarajuv2005 <lingarajuv2005@gmail.com>
2026-01-13 11:41:31 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,19 @@
+function login() {
+  const u = document.getElementById("username").value;
+  const p = document.getElementById("password").value;
+
+  if (
+    (u === "Lingaraju" && p === "Welcome@123") ||
+    (u === "Vishal" && p === "Welcome@123")
+  ) {
+    localStorage.setItem("currentUser", u);
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid Login");
+  }
+}
+
+function logout() {
+  localStorage.removeItem("currentUser");
+  window.location.href = "index.html";
+}
